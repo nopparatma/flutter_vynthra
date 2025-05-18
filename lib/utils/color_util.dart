8b25a776a6f61538696652faf7fe7c1b1bd559a6
Nopparat Manoi <yachty22541@gmail.com>
2025-05-18 23:22:40 +0700

    random color

--- lib/utils/color_util.dart
+++ lib/utils/color_util.dart
@@ -1,6 +1,18 @@
+import 'dart:math';
 import 'dart:ui';
 
+import 'package:get/get.dart';
+
 Color hexToColor(String hex) {
-  hex = hex.replaceAll("#", "");
+  if (hex.isBlank ?? false) {
+    final random = Random();
+    final r = random.nextInt(256).toRadixString(16).padLeft(2, '0');
+    final g = random.nextInt(256).toRadixString(16).padLeft(2, '0');
+    final b = random.nextInt(256).toRadixString(16).padLeft(2, '0');
+    hex = "$r$g$b";
+  } else {
+    hex = hex.replaceAll("#", "");
+  }
+
   return Color(int.parse("0xFF$hex"));
 }
